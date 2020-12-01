65966c2f7a04bead4e69b4239905f23b62318604
Emmanuel HERRMANN <emmanuel.herrmann@itsfactory.fr>
2020-12-01 11:24:34 +0100

    hopla ez

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let mut expenses:Vec<i32> = Vec::new();
+
+    let filename = "src/input";
+    // Open the file in read-only mode (ignoring errors).
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    // Read the file line by line using the lines() iterator from std::io::BufRead.
+    for line in reader.lines() {
+        let expense = line; // Ignore errors.
+        expenses.push(expense.unwrap().parse::<i32>().unwrap());
+    }
+
+    let mut expense1;
+    let mut expense2;
+
+    loop {
+        expense1 = expenses.pop().unwrap();
+        expense2 = expenses.iter().find(|e| *e + expense1 == 2020);
+        if expense2.is_some() { break; }
+    }
+
+    println!("{} {}", expense1, expense2.unwrap());
+
+    let result = expense1 * expense2.unwrap();
+    println!("{}", result);
 }
